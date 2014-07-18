94b1e61769a45e837ce7afa060de9ee90346f8b5
Jase2z <ighawvb@gmail.com>
2014-07-17 19:59:00 -0700

    Initial version

--- regex research/regex build.py
+++ regex research/regex build.py
@@ -7,29 +7,38 @@ class SkillLogTimeStamps:
     def __init__(self, skill_log_path):
         self.iter_counter = 1
         self.skill_log_path = skill_log_path
-        self.log_position = 0
+        self.log_position = [0]
         self.time_part = datetime.min.time()
         self.date_part = datetime.min.date()
+        self.datetime_list = [datetime.min, datetime.min]
 
     def __iter__(self):
         return self
 
     def __next__(self):
         with open(self.skill_log_path, encoding='utf-8') as fp:
-            fp.seek(self.log_position)
-            for line in fp:
+            fp.seek(self.log_position[0])
+            self.line_list = list()
+            for line in iter(fp.readline, ''):
+                self.log_position.insert(0, fp.tell())
+                self.log_position = self.log_position[:2]
                 line = line.strip()
-                result = time_stamp(line)
-                if result[0] is not None:
-                    self.date_part = result[0]
+                date1, time1 = time_stamp(line)
+                if date1 is not None:
+                    self.date_part = date1
                     continue
-                if result[1] is not None:
-                    self.time_part = result[1]
-                if self.date_part is None or self.time_part is None:
+                if time1 is not None:
+                    self.time_part = time1
+                if self.date_part.year == 1:
                     continue
-                datetime_stamp = datetime.combine(self.date_part, self.time_part)
-            raise StopIteration
-
+                self.datetime_list.insert(0, datetime.combine(self.date_part, self.time_part))
+                if self.datetime_list[1] == self.datetime_list[0] or self.datetime_list[1] == datetime.min:
+                    self.line_list.insert(0, line)
+                    continue
+                else:
+                    self.log_position = self.log_position[1:]
+                break
+        #raise StopIteration
     pass
 
 
@@ -41,13 +50,13 @@ def time_stamp(arg1):
     """
     if 'Logging' in arg1[:7]:
         _date_part = datetime.strptime(arg1[16:].strip(), "%Y-%m-%d").date()
-        return [_date_part, None]
+        return _date_part, None
     try:
         _time_part = datetime.strptime(arg1[:10], "[%H:%M:%S]").time()
     except ValueError:
-        return [None, None]
-    return [None, _time_part]
+        return None, None
+    return None, _time_part
 
 sk = SkillLogTimeStamps(skill_path)
-a = list()
-a.insert(0,'abc')
+for _ in sk:
+    print('{}\r\n'.format(sk.line_list))
