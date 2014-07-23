c8d002841f040227f820d826ccf5275084a4b14f
Jase2z <ighawvb@gmail.com>
2014-07-23 04:46:19 -0700

    Initial version

--- regex research/regex build.py
+++ regex research/regex build.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+import csv
+import re
 
 skill_path = 'C:/Users/Jason/Documents/wurm/players/joedobo/logs/_Skills.2014-03.txt'
+event_path = 'C:/Users/Jason/Documents/wurm/players/joedobo/logs/_Event.2014-03.txt'
 
 
 class SkillLogTimeStamps:
@@ -42,6 +45,42 @@ class SkillLogTimeStamps:
     pass
 
 
+class EventLog:
+    def __init__(self, event_log_path):
+        self.event_log_path = event_log_path
+        self.log_position = [0, 0]
+        self.time_part = datetime.min.time()
+        self.date_part = datetime.min.date()
+        self.datetime_list = [datetime.min, datetime.min]
+        self.match_time = datetime.min
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with open(self.event_log_path, encoding='utf-8') as fp:
+            fp.seek(self.log_position[0])
+            self.line_list = list()
+            for line in iter(fp.readline, ''):
+                self.log_position.insert(0, fp.tell())
+                self.log_position = self.log_position[:2]
+                line = line.strip()
+                #print(line)
+                date1, time1 = time_stamp(line)
+                if date1 is not None:
+                    self.date_part = date1
+                    continue
+                if time1 is not None:
+                    self.time_part = time1
+                if self.date_part.year == 1:
+                    continue
+                if self.match_time >= datetime.combine(self.date_part, self.time_part):
+                    self.line_list.insert(0, line)
+                if self.match_time < datetime.combine(self.date_part, self.time_part):
+                    self.log_position[0] = self.log_position[1]
+                    break
+
+
 def time_stamp(arg1):
     """
     Convert a string timestamp to datetime object.
@@ -57,6 +96,30 @@ def time_stamp(arg1):
         return None, None
     return None, _time_part
 
+
+def get_regex():
+    regex_list = list()
+    with open("regex.csv", encoding='utf-8', newline='') as fp:
+        csv_reader = csv.reader(fp, quoting=csv.QUOTE_NONE)
+        # b'\xe2\x99\xa0' = ♠ for utf-8
+        for row in csv_reader:
+            regex_list.append(row[0])
+        #print(regex_list)
+    return regex_list
+
+
+regex = get_regex()
 sk = SkillLogTimeStamps(skill_path)
+ev = EventLog(event_path)
 for _ in sk:
-    print('{}\r\n'.format(sk.line_list))
+    ev.match_time = sk.datetime_list[1]
+    next(ev)
+    found = False
+    for i in ev.line_list:
+        for i1 in regex:
+            result = re.search(i1, i)
+            if result is not None:
+                #print(i)
+                found = True
+    if found is False:
+        print('no match', sk.datetime_list[1])
